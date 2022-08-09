352cf51a23664858d04e87ab3bc264627a786613
JPCaillot <joaopcaillot@gmail.com>
2022-08-09 17:38:12 -0300

    Initial commit

---
+++ Módulo 1/cap14/script.js
@@ -0,0 +1,3 @@
+function Manutenção(){
+    window.alert ('O setor de compras está em manutenção. Pedimos desculpas pelo transtorno :(')
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# HTML5.CSS3
+ Curso de HTM5 CSS3 CursoemVideo
